fc58ea834a67cbb798aa37914e150fc34d961263
Pavel Bozdog <pbozdog@ucsd.edu>
2014-02-10 16:24:56 -0800

    Updated layout activity_more_info

--- src/com/wi14/team5/place_its/MainActivity.java
+++ src/com/wi14/team5/place_its/MainActivity.java
@@ -1,8 +1,11 @@
 package com.wi14.team5.place_its;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 	
@@ -10,13 +13,24 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+		
+		final Intent i = new Intent(MainActivity.this, MoreInfoActivity.class);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	    final Button button = (Button) findViewById(R.id.button1);
+	    
+	    button.setOnClickListener(new View.OnClickListener() {
+	        public void onClick(View v) {
+	        	System.out.println("hi");
+	            startActivity(i);
+	        }
+	    });
+	    
 	}
+	
+	public void buttonClick(View v) {
+		
+	}
+	
+	
 
 }
--- src/com/wi14/team5/place_its/MoreInfoActivity.java
+++ src/com/wi14/team5/place_its/MoreInfoActivity.java
@@ -1,7 +1,24 @@
 package com.wi14.team5.place_its;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.CancelableCallback;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MoreInfoActivity extends Activity {
-
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_more_info);
+		
+		
+	}
 }
