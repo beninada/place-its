ac32d43c6b8deedcefdf86a32a4ad68f6e17f3d4
rtominaga <richard.tominaga@gmail.com>
2014-02-13 23:28:10 -0800

    Added HashMap accessors

--- src/com/wi14/team5/place_its/PlaceIt.java
+++ src/com/wi14/team5/place_its/PlaceIt.java
@@ -2,55 +2,36 @@ package com.wi14.team5.place_its;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 import java.lang.String;
 
 public class PlaceIt {
 	private Marker marker;
-	private char reccurance;				//snippit, title, position
+	private char recurrence;
+	private int listNum;
 	
-	public PlaceIt(Marker marker, char reccurance){
+	public PlaceIt(Marker marker, char recurrence, int listNum){
 		this.marker     = marker;
-		this.reccurance = reccurance;
+		this.recurrence = recurrence;
+		this.listNum 	= listNum;
 	}
 	
 	public void writeOut(){
-		//TODO write object to database
 	}
 	
-	public void readIn(){
-		//TODO read object from database
+	public void readIn(){	
 	}
 	
-	public String getName(){
-		return this.marker.getTitle();
-	}
+	public String getName()							{return this.marker.getTitle();}
+	public LatLng getPosition()						{return this.marker.getPosition();}
+	public String getDescription()					{return this.marker.getSnippet();}
+	public Marker getMarker()						{return this.marker;}
+	public char getRecurrence()						{return this.recurrence;}
+	public int getListNum()							{return this.listNum;}
 	
-	public LatLng getPosition(){
-		return this.marker.getPosition();
-	}
-	
-	public String getDescription(){
-		return this.marker.getSnippet();
-	}
-	
-	public Marker getMarker(){
-		return this.marker;
-	}
-	
-	public void setName(String name){
-		this.marker.setTitle(name);
-	}
-	
-	public void setPosition(LatLng position){
-		this.marker.setPosition(position);
-	}
-	
-	public void setDescription(String description){
-		this.marker.setSnippet(description);
-	}
-	
-	public void setMarker(Marker marker){
-		this.marker = marker;
-	}
+	public void setName(String name)				{this.marker.setTitle(name);}
+	public void setPosition(LatLng position)		{this.marker.setPosition(position);}
+	public void setDescription(String description)	{this.marker.setSnippet(description);}
+	public void setMarker(Marker marker)			{this.marker = marker;}
+	public void setRecurrence(char reccurence)		{this.recurrence = recurrence;}
+	public void setListNum(int listNum)				{this.listNum = listNum;}
 }
